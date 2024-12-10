3fae12264d082eb68a6c9788134ba382d740f808
Meme Houeibib <38425746+memehoueibib@users.noreply.github.com>
2024-12-10 09:45:40 +0100

    Init

---
+++ EasyStudy/EasyStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyStudyApp.swift
+//  EasyStudy
+//
+//  Created by Utilisateur invité on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct EasyStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasyStudyTests/EasyStudyTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyStudyTests.swift
+//  EasyStudyTests
+//
+//  Created by Utilisateur invité on 10/12/2024.
+//
+
+import Testing
+@testable import EasyStudy
+
+struct EasyStudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
