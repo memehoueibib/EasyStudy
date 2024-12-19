3233293ac60df9e5c5182d01b0583c632c2dc61a
Meme Houeibib <38425746+memehoueibib@users.noreply.github.com>
2024-12-19 10:17:30 +0100

    Update SupabaseManager.swift

--- EasyStudy/Services/SupabaseManager.swift
+++ EasyStudy/Services/SupabaseManager.swift
@@ -7,8 +7,8 @@ class SupabaseManager {
     private let client: SupabaseClient
 
     private init() {
-        let supabaseURL = URL(string: "https://dbjarhbtrxqgdzebbaqy.supabase.co")! // Remplacez par votre URL Supabase
-        let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiamFyaGJ0cnhxZ2R6ZWJiYXF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM4MzQ4MDQsImV4cCI6MjA0OTQxMDgwNH0.dCEsLZNrtBz0_LvRc7Vffi2srJgrZwic-dELdyhcnYU" // Remplacez par votre clé Anon
+        let supabaseURL = URL(string: "")! 
+        let supabaseKey = "" 
         client = SupabaseClient(supabaseURL: supabaseURL, supabaseKey: supabaseKey)
     }
 
